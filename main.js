9e3401115c2ed79ab06d05b3fc808510ccb2c19c
Md Imon Hossain <imoncoc0@gmail.com>
2024-11-02 21:17:27 +0600

    reset button and functionality added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const sumElement = document.querySelector(".sum");
 const increaseButton = document.getElementById("increaseBtn");
 const decreaseButton = document.getElementById("decreaseBtn");
+const resetButton = document.getElementById("resetBtn");
 
 function createCalculator() {
   let sum = 0;
@@ -14,6 +15,10 @@ function createCalculator() {
       sum -= 1;
       return sum;
     },
+    reset: function () {
+      sum = 0;
+      return sum;
+    },
   };
 }
 
@@ -25,5 +30,9 @@ increaseButton.addEventListener("click", () => {
 });
 decreaseButton.addEventListener("click", () => {
   const newSum = calculator.subtract();
+  sumElement.textContent = newSum;
+});
+resetButton.addEventListener("click", () => {
+  const newSum = calculator.reset();
   sumElement.textContent = newSum;
 });
